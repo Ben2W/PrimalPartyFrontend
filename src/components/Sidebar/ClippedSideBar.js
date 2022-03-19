470581a7b1276a9e31fa9a45ba845e7d39d7c3e2
Aldrich Agabin <arcflu52@gmail.com>
2022-03-19 02:54:40 -0400

    Update Alt Text

--- src/components/Sidebar/ClippedSideBar.js
+++ src/components/Sidebar/ClippedSideBar.js
@@ -18,7 +18,7 @@ export default function ClippedDrawer() {
       <AppBar position="fixed" sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}>
         <Toolbar>
           <Typography variant="h6" noWrap component="div">
-          <img src={PrimalJetLogo}/ >
+            <img src={PrimalJetLogo} alt="PrimalJetLogo"/>
             PrimalParty
           </Typography>
         </Toolbar>
