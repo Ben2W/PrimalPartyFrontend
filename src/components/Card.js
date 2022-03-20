2cb813acca4ce046c995c6d7ac3341cfbba48034
Jonny Haldas <jonathan.haldas@gmail.com>
2022-03-20 17:42:58 -0400

    Made card uniform size

--- src/components/Card.js
+++ src/components/Card.js
@@ -9,32 +9,37 @@ import { purple } from '@mui/material/colors';
 
 export default function BasicCard(props){
 
-    const cardStyle={padding: 0, width:250, height:275, margin: "10px auto", display:'grid'}
+    const cardStyle={width:250, height: 275, display: 'flex', justifyContent: 'space-between', flexDirection: 'column'}
     const btnStyle={backgroundColor:"#3D4849"}
-    const descStyle={padding:'30px 0', fontWeight:600}
+    const descStyle={padding:'30px 0 0 0', fontWeight:600}
     
     return (
-        <Card style={cardStyle}>
-            <CardContent>
-                <div className="wrapper">
-                    <Typography variant="h5">
-                      {props.title}
-                    </Typography>
+        <Grid containter alignItems="stretch">
+            <Card style={cardStyle}>
+                <Grid>
+                    <CardContent>
+                        <div className="wrapper">
+                            <Typography variant="h5">
+                              {props.title}
+                            </Typography>
 
-                    <Typography variant="subtitle2">
-                      {props.date}
-                    </Typography>
-                </div>
+                            <Typography variant="subtitle2">
+                              {props.date}
+                            </Typography>
+                        </div>
 
-                <Typography variant="body2" color="text.primary" style={descStyle}>
-                  {props.desc}
-                </Typography>
-              </CardContent>
+                        <Typography variant="body2" color="text.primary" style={descStyle}>
+                          {props.desc}
+                        </Typography>
+                      </CardContent>
+                  </Grid>
 
-
-              <CardActions>
-                    <Button type='submit' color='primary' variant='contained' style={btnStyle}fullWidth >{props.button}</Button>
-              </CardActions>
-        </Card>
+                  <Grid>
+                      <CardActions>
+                            <Button type='submit' color='primary' variant='contained' style={btnStyle} fullWidth >{props.button}</Button>
+                      </CardActions>
+                  </Grid>
+            </Card>
+        </Grid>
     )
 }
