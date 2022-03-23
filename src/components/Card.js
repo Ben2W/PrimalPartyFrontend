d191ab5eaa88f58480424fafccaf27c8825ab7c4
Jonny Haldas <jonathan.haldas@gmail.com>
2022-03-22 20:10:46 -0400

    Added Modal to Card

--- src/components/Card.js
+++ src/components/Card.js
@@ -6,17 +6,40 @@ import CardContent from '@mui/material/CardContent';
 import AddCircleOutlineOutlinedIcon from '@mui/icons-material/AddCircleOutlineOutlined';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import Image from '../img/purple-grad.png'; // Import using relative path
+import Backdrop from '@mui/material/Backdrop';
+import Box from '@mui/material/Box';
+import Modal from '@mui/material/Modal';
+import Fade from '@mui/material/Fade';
+import Alert from '@mui/material/Alert';
+import AlertTitle from '@mui/material/Alert';
+
+
+const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+};
 
 export default function BasicCard(props){
 
     const cardStyle={width:250, height: 275, display: 'flex', justifyContent: 'space-between', flexDirection: 'column', backgroundImage: `url(${Image})`}
     const btnStyle={backgroundColor:"#000000"}
-    const descStyle={padding:'30px 0 0 0', color:'#F5F5F5'}
-    
+    const descStyle = { padding: '0 0 0 0', color: '#F5F5F5' }
+    const descStyleModal = { padding: '0 0 0 0', color: '#000000' }
+
+    const [open, setOpen] = React.useState(false);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
+
     return (
-        <Paper style={cardStyle.paperContainer}>
-            <Grid containter alignItems="stretch">
-                <Card style={cardStyle}>
+        <Grid containter alignItems="stretch">
+            <Card variant="outlined" style={cardStyle}>
                     <Grid>
                         <CardContent>
                             <div className="wrapper">
@@ -31,9 +54,12 @@ export default function BasicCard(props){
                                 <Typography variant="subtitle2" style={{color:'#F5F5F5'}}>
                                   Address: {props.address}
                                 </Typography>
+
+                                <Alert severity="info">You have # task(s)</Alert>
+
                             </div>
 
-                            <Typography variant="body2" color="text.light" style={descStyle}>
+                            <Typography variant="body2" style={descStyle}>
                               {props.desc}
                             </Typography>
 
@@ -41,12 +67,56 @@ export default function BasicCard(props){
                       </Grid>
 
                       <Grid>
-                          <CardActions>
-                                <Button type='submit' color='primary' variant='contained' style={btnStyle} fullWidth >{props.button}</Button>
-                          </CardActions>
-                      </Grid>
+                        <CardActions>
+                            <div>
+                                <Button onClick={handleOpen} type='submit' color='primary' variant='contained' style={btnStyle} fullWidth >{props.button}</Button>
+
+                                <Modal
+                                    aria-labelledby="transition-modal-title"
+                                    aria-describedby="transition-modal-description"
+                                    open={open}
+                                    onClose={handleClose}
+                                    closeAfterTransition
+                                    BackdropComponent={Backdrop}
+                                    BackdropProps={{
+                                        timeout: 500,
+                                    }}
+                                >
+                                    <Fade in={open}>
+                                        <Box sx={style}>
+                                            <Grid>
+                                                <CardContent>
+                                                    <div className="wrapper">
+                                                        <Typography variant="h5" style={{ color: '#000000' }}>
+                                                            {props.title}
+                                                        </Typography>
+
+                                                        <Typography variant="subtitle2" style={{ color: '#000000' }}>
+                                                            Date: {props.date}
+                                                        </Typography>
+
+                                                        <Typography variant="subtitle2" style={{ color: '#000000' }}>
+                                                            Address: {props.address}
+                                                        </Typography>
+                                                    </div>
+
+                                                    <Alert severity="info">
+                                                        Bring Pizza
+                                                    </Alert>
+
+                                                    <Typography variant="body2" style={descStyleModal}>
+                                                        {props.desc}
+                                                    </Typography>
+
+                                                </CardContent>
+                                            </Grid>
+                                        </Box>
+                                    </Fade>
+                                </Modal>
+                            </div>
+                        </CardActions>
+                     </Grid>
                 </Card>
             </Grid>
-        </Paper>
     )
 }
