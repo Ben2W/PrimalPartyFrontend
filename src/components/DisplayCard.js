0587988da5d2eb78a15357e3bbb4674b9aa6277b
Dylan Piper <Dylan_piper@knights.ucf.edu>
2022-03-25 19:34:51 -0400

    Adds working sidebar
    Props are sent to sidebar

---
+++ src/components/DisplayCard.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import Card from '../components/Card'
+
+const DisplayCard = () => {
+
+}
+
+export default DisplayCard
--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,35 +1,20 @@
-import React from 'react';
-import { Button } from '@material-ui/core';
+import React, { useEffect, useState } from 'react';
+import { Button } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
+import { Box } from '@mui/system';
+
+import HookedSideBar from '../components/Sidebar/HookedSideBar';
+import DisplayCard from '../components/Card'
 
 const Dashboard = () => {
-  let navigate = useNavigate();
+  const [user, setUser] = useState([])
 
-  const handleCreate = (e) => {
-    e.preventDefault();
+  useEffect(() => {
+    fetchAccount();
+  }, []);
 
-    navigate('/createparty');
-  }
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    fetch('http://localhost:8080/logout', {
-      method: 'POST',
-      headers: {
-          "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8"
-      },
-      credentials: 'include'
-    })
-    .then(response => response.json())
-    .then(data => {
-      console.log(data);
-      navigate('/');
-    })
-  }
-
-  const handleProtected = (e) => {
-    e.preventDefault();
-    fetch('http://localhost:8080/protected', {
+  const fetchAccount = async() => {
+     fetch('http://localhost:8080/account', {
       method: 'GET',
       headers: {
         "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8"
@@ -38,62 +23,124 @@ const Dashboard = () => {
     })
     .then(response => response.json())
     .then(data => {
-      console.log(data);
-    })
+      setUser(data.user)
+      // console.log(data);
+      // console.log(data.user);
+    })   
   }
 
-  const handleAccount = (e) => {
-    e.preventDefault();
-    fetch('http://localhost:8080/account', {
-      method: 'GET',
-      headers: {
-        "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8"
-      },
-      credentials: 'include',
-    })
-    .then(response => response.json())
-    .then(data => {
-      console.log(data);
-      navigate('/account');
-    })    
-  }
+ 
+
 
   return (
     <div className="dashboard">
-      <form onSubmit={ handleSubmit } >
-        <Button
-          type="submit"
-        >
-          Logout
-        </Button>
-        
-      </form>
-
-      <form onSubmit={handleProtected}>
-      <Button
-          type="submit"
-        >
-          Protected
-        </Button>
-      </form>
-
-      <form onSubmit={handleAccount}>
-      <Button
-          type="submit"
-        >
-          Account
-        </Button>
-      </form>
-
-      <form onSubmit={handleCreate}>
-        <Button
-          to='/createparty'
-        >
-          Create Party
-        </Button>
-      </form>
+      <HookedSideBar user = {user} />
+      <Box sx={{ marginLeft: 35, marginTop: 5}}>
+        <DisplayCard />
+      </Box>
     </div>
   );
 }
 
 export default Dashboard;
+
+       {/* <form onSubmit={ handleSubmit }>
+          <Button
+            type="submit"
+          >
+            Logout
+          </Button>
+        </form>
+
+        <form onSubmit={handleProtected}>
+          <Button
+            type="submit"
+          >
+            Protected
+          </Button>
+        </form>
+
+        <form onSubmit={handleAccount}>
+          <Button
+            type="submit"
+          >
+            Account
+          </Button>
+        </form>
+
+        <form onSubmit={handleCreate}>
+          <Button
+            to='/createparty'
+          >
+            Create Party
+          </Button>
+        </form> */}
+
+
+
+// const printEvent = () => {
+  //   for (var i=0; i<result.events.length; i++) {
+  //     <div>
+  //       result.events[i].name
+  //     </div>
+  //   }
+
+  // }
+
+  // const handleCreate = (e) => {
+  //   e.preventDefault();
+
+  //   navigate('/createparty');
+  // }
+
+  // const handleSubmit = (e) => {
+  //   e.preventDefault();
+  //   fetch('http://localhost:8080/logout', {
+  //     method: 'POST',
+  //     headers: {
+  //         "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8"
+  //     },
+  //     credentials: 'include'
+  //   })
+  //   .then(response => response.json())
+  //   .then(data => {
+  //     console.log(data);
+  //     navigate('/');
+  //   })
+  // }
+
+  //This is a how to see if the user is logged in
+  //
+  // const handleProtected = (e) => {
+  //   e.preventDefault();
+  //   fetch('http://localhost:8080/protected', {
+  //     method: 'GET',
+  //     headers: {
+  //       "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8"
+  //     },
+  //     credentials: 'include',
+  //   })
+  //   .then(response => response.json())
+  //   .then(data => {
+  //     console.log(data);
+  //   })
+  // }
+
+  //This is working example of a GET request
+  //
+  //
+  // const handleAccount = (e) => {
+  //   e.preventDefault();
+  //   fetch('http://localhost:8080/events', {
+  //     method: 'GET',
+  //     headers: {
+  //       "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8"
+  //     },
+  //     credentials: 'include',
+  //   })
+  //   .then(response => response.json())
+  //   .then(data => {
+  //     setResult(data)
+  //     // navigate('/account');
+  //   })    
+  // }
