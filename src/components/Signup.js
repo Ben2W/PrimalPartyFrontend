1a2b73a4286a7e714a70b6ef86622b10e14954a7
Dylan Piper <Dylan_piper@knights.ucf.edu>
2022-04-14 01:36:55 -0400

    Adds phone number formatting

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -51,6 +51,26 @@ const Signup = ({ handleChange }) => {
 
     let navigate = useNavigate();
 
+    const handlePhone = (e) => {
+        const formattedPhoneNumber = formatPhoneNumber(e.target.value);
+        setPhone(formattedPhoneNumber);
+    };
+
+    function formatPhoneNumber(value) {
+        const phoneNumber = value.replace(/[^\d]/g, '');
+        const phoneNumberLength = phoneNumber.length;
+
+        if (phoneNumberLength < 4) return phoneNumber;
+        if (phoneNumberLength < 7) {
+            return `(${phoneNumber.slice(0, 3)}) ${phoneNumber.slice(3)}`;
+        }
+        return `(${phoneNumber.slice(0, 3)}) ${phoneNumber.slice(
+            3,
+            6,
+
+        )}-${phoneNumber.slice(6, 10)}`;
+    }
+
     const handleSubmit = (e) => {
         e.preventDefault();
         setErrorMessage('')
@@ -176,7 +196,7 @@ const Signup = ({ handleChange }) => {
                             fullWidth
                             label="Enter Phone Number"
                             value={phone}
-                            onChange={(e) => setPhone(e.target.value)}
+                            onChange={(e) => handlePhone(e)}
                         /><br />
 
                         {!isPending && <Button
