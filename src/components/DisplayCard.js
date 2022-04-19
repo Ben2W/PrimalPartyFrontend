f6547038cf596e204df24c729a6e1cf6f9a571d0
Dylan Piper <Dylan_piper@knights.ucf.edu>
2022-04-19 01:47:48 -0400

    updates

--- src/components/DisplayCard.js
+++ src/components/DisplayCard.js
@@ -1,16 +1,43 @@
-import { Typography } from '@mui/material'
-import React from 'react';
+import { Grid, Typography } from '@mui/material'
+import React, { useContext, useState } from 'react';
 import DisplayCardHelper from '../components/DisplayCardHelper'
 import { UserContext } from '../context/UserContext';
+import { Button } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
 
 const DisplayCard = () => {
+    const { user } = useContext(UserContext);
+    const navigate = useNavigate();
+    console.log(user.events.length);
+    if (user.events.length == 0) {
+        return (
+            <div>
+                <Grid container>
+                    <Grid item sx={6}>
+                        <Typography variant='h5' sx={{ paddingBottom: 3, fontWeight: 'bold', color: '#ffffff' }}>Looks like there are no events...</Typography>
+                        <Button
+                            sx={{ fontSize: '18px', fontWeight: 600 }}
+                            type='submit'
+                            onClick={() => navigate('/createparty')}
+                            size='large'
+                            variant='contained'
+                            fullWidth
+                        >
+                            Create an event to get started
+                        </Button>
+                    </Grid>
+                </Grid>
 
-    return (
-        <div>
-            <Typography variant='h3' sx={{paddingBottom: 3, fontWeight: 'bold', color: '#ffffff'}}>Upcoming Events</Typography>
-            <DisplayCardHelper />
-        </div>
-    )
+            </div>
+        )
+    } else {
+        return (
+            <div>
+                <Typography variant='h3' sx={{ paddingBottom: 3, fontWeight: 'bold', color: '#ffffff' }}>Upcoming Events</Typography>
+                <DisplayCardHelper />
+            </div>
+        )
+    }
 }
 
 export default DisplayCard
